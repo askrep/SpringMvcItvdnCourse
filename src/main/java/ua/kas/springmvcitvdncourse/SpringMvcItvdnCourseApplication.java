f586667bdfe4370b38a6d50eaf55f952869fb022
Alexander <alexandskr@gmail.com>
2021-02-22 15:10:56 +0200

    lesson#3 Hello world

---
+++ src/main/java/ua/kas/springmvcitvdncourse/SpringMvcItvdnCourseApplication.java
@@ -0,0 +1,13 @@
+package ua.kas.springmvcitvdncourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "ua.kas.springmvcitvdncourse")
+public class SpringMvcItvdnCourseApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringMvcItvdnCourseApplication.class);
+    }
+}
---
+++ src/main/java/ua/kas/springmvcitvdncourse/controllers/HelloWorldController.java
@@ -0,0 +1,16 @@
+package ua.kas.springmvcitvdncourse.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+
+    @GetMapping(path = {"/", "/hello"})
+    public String getHelloWorld(Model model, @RequestParam(value = "name") String name) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
